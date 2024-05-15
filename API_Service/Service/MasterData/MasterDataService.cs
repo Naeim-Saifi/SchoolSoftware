751d8c17846504e2e8ffb35c0003157b48898146
sakib ansari <sakibansari94110@gmail.com>
2024-05-15 16:15:37 +0530

    added class and section

---
+++ API_Service/Service/MasterData/MasterDataService.cs
@@ -0,0 +1,117 @@
+﻿//using AdminDashboard.Server.API_Service.Interface.MasterData;
+//using AdminDashboard.Server.Service.Interface.LocalStorage;
+//using AIS.Data.APIReturnModel;
+//using AIS.Data.Model;
+//using AIS.Data.RequestResponseModel.Enquiry;
+//using AIS.Data.RequestResponseModel.MasterData.MasterConfiguration.Class;
+//using AIS.Data.RequestResponseModel.MasterData.MasterConfiguration.Section;
+//using Microsoft.AspNetCore.Components;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Serialization;
+//using Syncfusion.Blazor.Charts.Chart.Internal;
+//using System;
+//using System.Collections.Generic;
+//using System.Net.Http;
+//using System.Text.Json;
+//using System.Threading.Tasks;
+
+//namespace AdminDashboard.Server.API_Service.Service.MasterData
+//{
+//    public class MasterDataService : IMasterDataService
+//    {
+//        private readonly HttpClient httpClient;
+//        private ILocalStorageService _localStorageService;
+
+//        public MasterDataService(HttpClient httpClient, ILocalStorageService localStorageService)
+//        {
+//            this.httpClient = httpClient;
+//            _localStorageService = localStorageService;
+//        }
+
+//        public async Task<APIReturnModel> CRUD_ClassDetails(ClassApiModel classApiModel)
+//        {
+//            APIReturnModel response = await httpClient.PostJsonAsync<APIReturnModel>("", classApiModel);
+//            return response;
+//        }
+
+//        public async Task<IEnumerable<ClassOutputModel>> GET_ClassDetails_List(ClassParaModel classParaModel)
+//        {
+//            try
+//            {
+//                JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+//                {
+//                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+//                };
+
+//                var r = await httpClient.GetJsonAsync<Response>($"  ?SchoolCode={classParaModel.schoolCode}" +
+//                    $"&FinancialYear={classParaModel.financialYear}" +
+//                    $"&Classid={classParaModel.ClassId}" +
+//                    $"&RoleID={classParaModel.userRoleId}" +
+//                    $"&ReportType={classParaModel.reportType}");
+//                JsonElement el;
+//                var b = System.Text.Json.JsonSerializer.Deserialize<JsonElement>(r.Data.GetRawText()).TryGetProperty("data", out el);
+
+//                if (r.IsError == false)
+//                {
+//                    return System.Text.Json.JsonSerializer.Deserialize<ClassOutputModel[]>(el.GetRawText());
+//                }
+//                else
+//                {
+//                    return null;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//            return null;
+
+//        }
+
+
+
+
+//        public async Task<APIReturnModel> CRUD_SectionDetails(SectionApiModel sectionApiModel)
+//        {
+//            APIReturnModel response = await httpClient.PostJsonAsync<APIReturnModel>("", sectionApiModel);
+//            return response;
+//        }
+
+
+
+//        public async Task<IEnumerable<SectionOutputModel>> GET_SectionDetails_List(SectionParaModel sectionParaModel)
+//        {
+//            try
+//            {
+//                JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+//                {
+//                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+//                };
+
+//                var r = await httpClient.GetJsonAsync<Response>($"  ?SchoolCode={sectionParaModel.schoolCode}" +
+//                    $"&FinancialYear={sectionParaModel.financialYear}" +
+//                    $"&Classid={sectionParaModel.SectionId}" +
+//                    $"&RoleID={sectionParaModel.userRoleId}" +
+//                    $"&ReportType={sectionParaModel.reportType}");
+//                JsonElement el;
+//                var b = System.Text.Json.JsonSerializer.Deserialize<JsonElement>(r.Data.GetRawText()).TryGetProperty("data", out el);
+
+//                if (r.IsError == false)
+//                {
+//                    return System.Text.Json.JsonSerializer.Deserialize<SectionOutputModel[]>(el.GetRawText());
+//                }
+//                else
+//                {
+//                    return null;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+//            return null;
+
+//        }
+
+//    }
+//}
--- User_Pages/MasterData/MasterConfiguration/SectionBase.cs
+++ User_Pages/MasterData/MasterConfiguration/SectionBase.cs
@@ -1,8 +1,8 @@
-﻿
+﻿//using AdminDashboard.Server.API_Service.Service.Inventory;
 using AIS.Data.APIReturnModel;
 using AIS.Data.RequestResponseModel.Enquiry;
 using AIS.Data.RequestResponseModel.Inventory.ItemMaster;
-
+//using AIS.Data.RequestResponseModel.Inventory.ItemVender;
 using AIS.Model.GeneralConversion;
 using AIS.Model.UserLogin;
 using Microsoft.AspNetCore.Components;
@@ -13,110 +13,282 @@ using Syncfusion.Blazor.Popups;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
-
+//using AdminDashboard.Server.API_Service.Interface.Inventory;
 using System.Linq;
+using AIS.Data.RequestResponseModel.MasterData.MasterConfiguration.Class;
+using AIS.Data.RequestResponseModel.MasterData.MasterConfiguration.Section;
+//using AdminDashboard.Server.API_Service.Interface.MasterData;
+
 
 namespace AdminDashboard.Server.User_Pages.MasterData.MasterConfiguration
 {
     public class SectionBase : ComponentBase
     {
-
-        public List<ItemMasterListMoel> _ItemMasterListMoel = new List<ItemMasterListMoel>();
-        public SfGrid<ItemMasterListMoel> sfItemDetails;
-
-        [Inject]
-        Blazored.SessionStorage.ISessionStorageService session { get; set; }
-        public SessionModel _sessionModel;
-
-        [Inject]
-        public ISnackbar snackBar { get; set; }
+   
 
 
-        public DialogEffect AnimationEffect = DialogEffect.Zoom;
-        public string HeaderStyles { get; set; } = "e-background e-accent";
-        public SfDialog DialogRef;
 
-        List<string> IList = new List<string>();
+            public SectionViewModel sectionViewModel = new SectionViewModel();
 
 
-        public bool IsVisible { get; set; } = false;
-        public bool IsDeleteVisible { get; set; } = false;
-        public string DialogHeaderName = string.Empty;
-        public bool ddEnable = true;
-        public string btncss = "";
-        public string HeaderText = string.Empty;
-        public string OperationType = "";
-
-        public APIReturnModel aPIReturnModel;
+            public List<SectionOutputModel> _SectionListModel = new List<SectionOutputModel>();
+            public SfGrid<SectionOutputModel> sfSectionDetails;
+            public SectionApiModel sectionApiModel { get; set; }
 
 
-        public List<object> MenuItems = new List<object>()
+
+            [Inject]
+            Blazored.SessionStorage.ISessionStorageService session { get; set; }
+            public SessionModel _sessionModel;
+
+            [Inject]
+            public ISnackbar snackBar { get; set; }
+
+           //[Inject]
+           //  public IMasterDataService masterDataService { get; set; }
+
+            public DialogEffect AnimationEffect = DialogEffect.Zoom;
+            public string HeaderStyles { get; set; } = "e-background e-accent";
+            public SfDialog DialogRef;
+
+            List<string> IList = new List<string>();
+
+
+            public bool IsVisible { get; set; } = false;
+            public bool IsDeleteVisible { get; set; } = false;
+            public string DialogHeaderName = string.Empty;
+            public bool ddEnable = true;
+            public string btncss = "";
+            public string HeaderText = string.Empty;
+            public string OperationType = "";
+
+            public APIReturnModel aPIReturnModel;
+
+
+            public List<object> MenuItems = new List<object>()
             { "AutoFit", "AutoFitAll", "SortAscending", "SortDescending",
                 "Copy", "ExcelExport", "CsvExport", "FirstPage", "PrevPage", "LastPage", "NextPage"
             };
 
-        public List<string> EnquirytoolBarItems = (new List<string>() { "Add I", "Print", "ExportExcel", "Collapse All", "Expand All", "Search" });
+            public List<string> ToolBarItems = (new List<string>() { "Add Section", "Print", "ExportExcel", "Collapse All", "Expand All", "Search" });
 
 
 
 
+            public class SectionName
+            {
+                public int Id;
+                public string Value;
+            }
 
-
-
-        protected override async Task OnInitializedAsync()
+            public List<SectionName> sectionNamedetails = new List<SectionName>()
         {
-            //_sessionModel = await session.GetItemAsync<SessionModel>("sessionUser");
-            ////Master_CLass_List_Input_Para_Model master_CLass_List_Input_Para_Model = new Master_CLass_List_Input_Para_Model()
-            ////{
-            ////    classId = 0,
-            ////    userId = _sessionModel.UserId,
-            ////    financialYear = _sessionModel.FinancialYear,
-            ////    schoolCode = _sessionModel.SchoolCode,
-            ////    reportType = ReportType.All
-            ////};
-            ////_classList = (await masterDataSetupService.GET_Master_ClassLIST(master_CLass_List_Input_Para_Model)).ToList();
+            new SectionName{Id=1,Value="A"},
+             new SectionName{Id=2,Value="B"},
+        new SectionName{Id=3,Value="C"},
+           new SectionName{Id=4,Value="D"}
+   
 
-            //ItemMasterParaModel itemMasterParaModel = new ItemMasterParaModel()
+        };
+
+
+
+
+
+
+            protected override async Task OnInitializedAsync()
+            {
+                _sessionModel = await session.GetItemAsync<SessionModel>("sessionUser");
+                //Master_CLass_List_Input_Para_Model master_CLass_List_Input_Para_Model = new Master_CLass_List_Input_Para_Model()
+                //{
+                //    classId = 0,
+                //    userId = _sessionModel.UserId,
+                //    financialYear = _sessionModel.FinancialYear,
+                //    schoolCode = _sessionModel.SchoolCode,
+                //    reportType = ReportType.All
+                //};
+                //_classList = (await masterDataSetupService.GET_Master_ClassLIST(master_CLass_List_Input_Para_Model)).ToList();
+
+                SectionParaModel sectionParaModel = new SectionParaModel()
+                {
+                    financialYear = _sessionModel.FinancialYear,
+                    schoolCode = _sessionModel.SchoolCode,
+                    SectionId = 0,
+                    userRoleId = _sessionModel.RoleId,
+                    reportType = ReportType.All
+                };
+
+                 //   _SectionListModel = (await masterDataService.GET_SectionDetails_List(sectionParaModel)).ToList();
+            }
+
+
+
+
+
+
+
+            public void EditItemDetail(CommandClickEventArgs<SectionOutputModel> args)
+        {
+
+
+            // Perform required operations here
+            string buttontext = args.CommandColumn.ButtonOption.Content;
+            //int testId = args.RowData.testID;
+            if (buttontext == "Edit")
+            {
+                //navigationManager.NavigateTo($"/OnlineExam/ViewResult/{ testId}");
+                IsVisible = true;
+                DialogHeaderName = "Update Section Details";
+                HeaderText = "Update Record";
+                OperationType = "Update";
+                btncss = "e-flat e-info e-outline";
+                // ddEnable = false;
+                sectionViewModel = new SectionViewModel()
+                {
+                    sectionId = args.RowData.sectionId,
+                    sectionCode = args.RowData.sectionCode,
+                    sectionName = args.RowData.sectionName,
+                    displayOrder = args.RowData.displayOrder,
+
+
+                };
+            }
+            else
+            {
+                IsVisible = true;
+                DialogHeaderName = "Delete Section Details";
+                OperationType = "Delete";
+                HeaderText = "Delete Record";
+                btncss = "e-flat e-danger e-outline";
+                ddEnable = false;
+                sectionViewModel = new SectionViewModel()
+                {
+
+                    sectionId = args.RowData.sectionId,
+                    sectionCode = args.RowData.sectionCode,
+                    sectionName = args.RowData.sectionName,
+                    displayOrder = args.RowData.displayOrder,
+
+                };
+            }
+            //else
             //{
-            //    financialYear = _sessionModel.FinancialYear,
-            //    schoolCode = _sessionModel.SchoolCode,
-            //    ItemId = 0,
-            //    userRoleId = _sessionModel.RoleId,
-            //    reportType = ReportType.All
-            //};
-
-            // _ItemVenderListModel = (await enquiryService.GET_EnquiryDetails_List(itemMasterParaModel)).ToList();
-        }
-
-
-
-
-
-
-
-        public void EditItemDetail(CommandClickEventArgs<ItemMasterListMoel> args)
-        {
-
+            //    navigationManager.NavigateTo($"/OnlineExam/OnlineTestDetails/{testId}");
+            //}
 
         }
 
 
         public void ToolbarClickHandler(Syncfusion.Blazor.Navigations.ClickEventArgs args)
-        {
-            if (args.Item.Text == "Add I")
             {
-                //navigationManager.NavigateTo("/OnlineExam/TestList");
-                //perform your actions here
-                IsVisible = true;
-                OperationType = "";
-                btncss = "e-flat e-primary e-outline";
-                DialogHeaderName = "Add  Details";
-                OperationType = "Add";
-                HeaderText = "Add";
-                ddEnable = true;
+                if (args.Item.Text == "Add Section")
+                {
+                    //navigationManager.NavigateTo("/OnlineExam/TestList");
+                    //perform your actions here
+                    IsVisible = true;
+                    OperationType = "";
+                    btncss = "e-flat e-primary e-outline";
+                    DialogHeaderName = "Add Section Details";
+                    OperationType = "Add";
+                    HeaderText = "Add Section";
+                    ddEnable = true;
+
+                }
+
+
+            else if (args.Item.Text == "ExportExcel")
+            {
+                this.sfSectionDetails.ExportToExcelAsync();
+            }
+            else if (args.Item.Text == "Collapse All")
+            {
+                this.sfSectionDetails.CollapseAllGroupAsync();
+            }
+            else if (args.Item.Text == "Expand All")
+            {
+                this.sfSectionDetails.ExpandAllGroupAsync();
+            }
+        }
+
+
+
+
+            public async void OnValidSubmit(EditContext contex)
+            {
+                bool isValid = contex.Validate();
+                if (isValid)
+                {
+                sectionApiModel = new SectionApiModel()
+                    {
+                        SectionName = sectionViewModel.sectionName,
+                        SectionCode = sectionViewModel.sectionCode,
+                        DisplayOrder = sectionViewModel.displayOrder,
+
+
+
+
+                    };
+                    if (OperationType == "Add")
+                    {
+                        sectionApiModel.OperationType = OperationAction.Add;
+                    }
+                    else if (OperationType == "Update")
+                    {
+                    sectionApiModel.OperationType = OperationAction.Update;
+                    }
+                    //Delete Operation
+                    else
+                    {
+                    sectionApiModel.OperationType = OperationAction.Delete;
+                    }
+
+                    SaveItemMasterDetails(sectionApiModel);
+
+                }
+                else
+                {
+                    // Form has invalid inputs.
+                }
 
             }
 
+            private async void SaveItemMasterDetails(SectionApiModel sectionApiModel)
+            {
+            try
+            {
+                if (sectionApiModel.OperationType != "NA")
+                {
+                   //    aPIReturnModel = await masterDataService.CRUD_SectionDetails(sectionApiModel);
+
+                    if (aPIReturnModel.status == false)
+                    {
+                        snackBar.Add(aPIReturnModel.message, Severity.Success);
+
+
+                        SectionParaModel sectionParaModel = new SectionParaModel()
+                        {
+                            financialYear = _sessionModel.FinancialYear,
+                            schoolCode = _sessionModel.SchoolCode,
+                            SectionId = 0,
+                            userRoleId = _sessionModel.RoleId,
+                            reportType = ReportType.All
+                        };
+
+                     //    _SectionListModel  = (await masterDataService.GET_SectionDetails_List(sectionParaModel)).ToList();
+
+                        ClearData();
+                        StateHasChanged();
+                    }
+                    else
+                    {
+                        snackBar.Add(aPIReturnModel.message, Severity.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
 
 
@@ -126,25 +298,27 @@ namespace AdminDashboard.Server.User_Pages.MasterData.MasterConfiguration
 
 
 
+            private void ClearData()
+            {
+            sectionViewModel = new SectionViewModel();
+
+            }
 
 
+            public void ShowDialog()
+            {
+                IsVisible = true;
+            }
+            public void onOpen(Syncfusion.Blazor.Popups.BeforeOpenEventArgs args)
+            {
+                // setting maximum height to the Dialog
+                args.MaxHeight = "750px";
 
-
-
-        public void ShowDialog()
-        {
-            IsVisible = true;
-        }
-        public void onOpen(Syncfusion.Blazor.Popups.BeforeOpenEventArgs args)
-        {
-            // setting maximum height to the Dialog
-            args.MaxHeight = "750px";
-
-        }
-        public async Task CloseDialog()
-        {
-            IsVisible = false;
-            await this.DialogRef.HideAsync();
+            }
+            public async Task CloseDialog()
+            {
+                IsVisible = false;
+                await this.DialogRef.HideAsync();
+            }
         }
     }
-}
